62968053dd125e324a348713e98b75153e62cd1c
edgar <edgar@DESKTOP-DOF0P7M>
2020-03-29 05:24:55 -0300

    trab 1

---
+++ Trab1/src/trab1/PilotoTester.java
@@ -0,0 +1,25 @@
+package trab1;
+
+public class PilotoTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Piloto c1;
+		c1 = new Piloto();
+		c1.nome ="Joao";
+		c1.idade = 23;
+		c1.segurandoguidao = true;
+		
+		//c1.status();
+	
+        
+       Piloto c2;
+       	c2= new Piloto();
+		c2.nome ="Jose";
+		c2.idade = 50;
+		c2.segurandoguidao = true;
+		c2.status();
+	}
+
+}
